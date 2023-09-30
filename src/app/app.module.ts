1e56fbfdace6b9362ccabab9341763bfda770a74
ThiagoLeto <81710270+ThiagoLeto@users.noreply.github.com>
2023-09-30 20:53:04 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { SeccionComponent } from './pages/seccion/seccion.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ComparadorComponent } from './pages/comparador/comparador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SeccionComponent,
     MenuComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    ComparadorComponent
   ],
   imports: [
     BrowserModule,
