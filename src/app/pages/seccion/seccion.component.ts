073556b48607e1d5d3590fb94d5fb79140d1ddce
valentinogarcia <81708746+valentinogarcia@users.noreply.github.com>
2023-10-23 17:38:41 -0300

    Add files via upload

--- src/app/pages/seccion/seccion.component.ts
+++ src/app/pages/seccion/seccion.component.ts
@@ -9,11 +9,44 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class SeccionComponent {
   data:any[]=[];
+  tagsFinales:any[]=[]
+
   constructor(
     private router: Router,private api:ApiService
-  ) { console.log(api.getTags().subscribe(dat=>{ console.log( dat[1].nombre );
-    this.data.push(dat);
+  ) { 
+    console.log(api.getTags().subscribe((dat: any[])=>{ 
+    dat.forEach((element: any) => {
+      this.data.push(element);
+
+    })
+    
+    this.data.forEach(async(element: any) => {
+      element.subsecciones.forEach(async (x:any) => {
+        await this.GetFinales(x)
+      });
+    }); console.log(this.tagsFinales[0]);
+    ;
   }) );
+   }
+
+
+   async GetFinales(x:any) {
+    if(!x.finales && !x.subsecciones){
+      console.log(x.img)
+      this.tagsFinales.push(x );
+    }
+    else{
+      if(x.finales){
+        x.finales.forEach(async(y: any)=> {
+          await this.GetFinales(y);
+        });
+      }
+      if (x.subsecciones){
+        x.subsecciones.forEach(async(y: any)=> {
+          await this.GetFinales(y.nombre);
+        });
+      }
+    }
    }
   
   comparadorButton(){
